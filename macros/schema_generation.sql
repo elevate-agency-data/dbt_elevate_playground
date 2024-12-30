4da0cc697c321cef17ced26ba0212a359c39c3c2
eric.zulficar <eric.zulficar@elevate-agency.com>
2024-12-30 11:34:32 +0100

    adding username when creating a schema

--- macros/schema_generation.sql
+++ macros/schema_generation.sql
@@ -1,10 +1,12 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
+    {% set username = var('dbt_username') %}
     {%- set default_schema = target.schema -%} 
     {%- if custom_schema_name is none -%}
         {%- set model_path_str = node.path | string -%}
         {%- set normalized_path = model_path_str.replace('\\', '/') -%}
         {%- set folder_name = normalized_path.split('/')[-2] -%}
-        {{ folder_name }}
+        {{ log("Folder name : " ~ normalized_path) }}
+        {{ username }}_{{ folder_name }}
     {%- else -%}
         {{ default_schema }}_{{ custom_schema_name | trim }}
     {%- endif -%}
